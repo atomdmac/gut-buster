bd5e704d7dac59a3cd0db1d6f023e9f847559a38
atom <adam@macumber.io>
2024-08-25 18:46:15 -0400

    Include lib files 'cause like... it's just easier for Github pages.

--- src/main.js
+++ src/main.js
@@ -5,9 +5,8 @@
         baseUrl: "src/",
         
         paths: {
-            //  Edit the below path to point to where-ever you have placed the phaser.min.js file
-            phaser: 'lib/phaser/build/phaser.min',
-            'phaser-transition': 'lib/phaser-state-transition-plugin/dist/phaser-state-transition-plugin.min'
+            'phaser': 'lib/phaser.min',
+            'phaser-transition': 'lib/phaser-state-transition-plugin.min'
         },
 
         shim: {
